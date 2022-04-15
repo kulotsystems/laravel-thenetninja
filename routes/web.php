f72723843fc08c0a9be1bdf485a0ee3dc3423320
kulotsystems <kulotsystems@gmail.com>
2022-04-15 17:53:42 +0800

    Lesson 27: Disabling Registration

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::post  ('/pizzas'       , [PizzaController::class, 'store']);
 Route::get   ('/pizzas/{id}'  , [PizzaController::class, 'show'])->middleware('auth');
 Route::delete('/pizzas/{id}'  , [PizzaController::class, 'destroy'])->middleware('auth');
 
-Auth::routes();
+Auth::routes([
+    'register' => false
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
