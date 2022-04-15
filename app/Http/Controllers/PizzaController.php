0fbe52ab8a3823d67ba272ac23f110d8f7d3bfe3
kulotsystems <kulotsystems@gmail.com>
2022-04-15 16:15:36 +0800

    Lesson 20: Saving Records

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -40,10 +40,14 @@ class PizzaController extends Controller
 
     public function store()
     {
-        error_log(request('name'));
-        error_log(request('type'));
-        error_log(request('base'));
+        $pizza = new Pizza();
+        $pizza->name  = request('name');
+        $pizza->type  = request('type');
+        $pizza->base  = request('base');
+        $pizza->price = 0;
+        $pizza->save();
 
-        return redirect('/');
+        return redirect('/')
+            ->with('mssg', 'Thanks for your order.');
     }
 }
