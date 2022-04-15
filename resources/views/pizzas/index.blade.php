31b5979b50186be33e32d7e7744563a19977b389
kulotsystems <kulotsystems@gmail.com>
2022-04-15 17:19:12 +0800

    Lesson 23: Using SASS

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,20 +1,13 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            Pizza List
+<div class="wrapper pizza-index">
+    <h1>Pizza Orders</h1>
+    @foreach($pizzas as $pizza)
+        <div class="pizza-item">
+            <img src="/img/pizza.png" alt="Pizza Icon">
+            <h4><a href="/pizzas/{{ $pizza->id }}">{{ $pizza->name }}</a></h4>
         </div>
-
-        <p>{{ $name }}</p>
-        <p>{{ $age }}</p>
-
-        @foreach($pizzas as $pizza)
-            <div>
-                {{ $pizza->name }} - {{ $pizza->type }} - {{ $pizza->base }}
-            </div>
-        @endforeach
-    </div>
+    @endforeach
 </div>
 @endsection
