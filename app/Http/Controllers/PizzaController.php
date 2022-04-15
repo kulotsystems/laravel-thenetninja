974cc4f9e72dc4b305f5e27dd07b924b845728d1
kulotsystems <kulotsystems@gmail.com>
2022-04-15 14:53:53 +0800

    Lesson 15: Eloquent Models

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,25 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pizza;
 
 class PizzaController extends Controller
 {
     public function index()
     {
-        $pizzas = [
-            [
-                'type' => 'hawaiian',
-                'base' => 'cheesy crust'
-            ],
-            [
-                'type' => 'volcano',
-                'base' => 'garlic crust'
-            ],
-            [
-                'type' => 'veg supreme',
-                'base' => 'thin & crispy'
-            ],
-        ];
+        // $pizzas = Pizza::all();
+        // $pizzas = Pizza::orderBy('name', 'desc')->get();
+        // $pizzas = Pizza::where('type', 'hawaiian')->get();
+        $pizzas = Pizza::latest()->get();
 
         return view('pizzas', [
             'pizzas' => $pizzas,
