639478fb6a153bcd7fa9853b8d73ac7f7512b730
kulotsystems <kulotsystems@gmail.com>
2022-04-15 10:09:09 +0800

    Lesson 3: Laravel Basics (Routes, Views)

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -81,19 +81,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Pizza House<br />
-            The North's Best Pizzas
-        </div>
-
-        <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
+            Pizzas
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    return view('pizzas');
+});
